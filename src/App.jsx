30cf11948dc684b8dc7ea06f7dfa4bb406fa092d
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-27 22:17:10 +0500

    initialized usestates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+const API_URL = 'http://localhost:3000/api/users'
 
 function App() {
+
+  const [users, setUsers]  = useState([])
+ const [newUser, setNewUser] = useState('');
+ const [updateUser, setUpdateUser] = useState({ id: '', name: '' });
+
+
   return (
+
   )
 }
 
